65a3d65044b6323dc16df3f7c09430d3878715bc
giacoppo <giacoppo@gmail.com>
2017-10-17 17:19:48 +0200

    Node JS File System

---
+++ directory.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+if(fs.existsSync("lib2")) {
+    console.log("Directory allready there");
+} else {
+    fs.mkdir("lib2", function(err) {
+
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Directory Created");
+        }
+
+    });
+}
+
